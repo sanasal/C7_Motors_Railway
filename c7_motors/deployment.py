a61b252a9ddb75b99b89e6df2f4aa941ae784a99
sanasalhdar@gmail.com <sanasalhdar@gmail.com>
2025-03-10 12:38:28 +0200

    change the port

--- c7_motors/deployment.py
+++ c7_motors/deployment.py
@@ -95,7 +95,7 @@ else:
                 'USER': os.environ.get('MYSQL_USER'),
                 'PASSWORD':os.environ.get('MYSQL_PASSWORD'),
                 'HOST': os.environ.get('MYSQL_HOST'),
-                'PORT': os.environ.get('MYSQL_PORT') ,
+                'PORT': '3306' ,
                 'ssl_disabled': True,
                 'OPTIONS': {
                 'connect_timeout': 10,
